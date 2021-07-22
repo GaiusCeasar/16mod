220f18fb9f7d8f6ad62c88af4ee6484f06015636
Maksim <85397052+GaiusCeasar@users.noreply.github.com>
2021-07-22 15:42:36 +0300

    16.7.1

---
+++ testCat.py
@@ -0,0 +1,13 @@
+from Cat import Cat
+
+c1 = Cat('Барон', 'Мальчик', '2 года')
+
+print('Имя', c1.name)
+print('Пол', c1.gender)
+print('Возраст', c1.age)
+
+c2 = Cat('Сэм', 'Мальчик', '2 года')
+
+print('Имя', c2.name)
+print('Пол', c2.gender)
+print('Возраст', c2.age)
